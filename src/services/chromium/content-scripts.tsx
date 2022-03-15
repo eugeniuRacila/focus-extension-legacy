7b619add3adb93619466f9fb5c50056fecfd8962
Eugeniu Racila <eugeniu.racila@gmail.com>
2022-03-15 16:08:16 +0100

    Add focus element text styles

--- src/services/chromium/content-scripts.tsx
+++ src/services/chromium/content-scripts.tsx
@@ -2,7 +2,7 @@ import { injectFocusElement } from '../../core/inject';
 import { observeBodyCreation } from '../../utils/dom-mutation-observer';
 
 // Inject Focus JSX element
-if (window.location.toString().includes('google.com')) {
+if (window.location.toString().includes('unsplash.com')) {
   observeBodyCreation().then((body) => {
     injectFocusElement(body);
   });
